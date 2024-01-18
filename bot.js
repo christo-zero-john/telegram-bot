0cdece770bca799b9cb775e55384d077fd64ecd3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-01-18 15:42:29 +0530

    commit

--- bot.js
+++
@@ -1,9 +0,0 @@
-const botToken = '6374135626:AAFk_xQaV1_G3YYsS18kFQ2y509x1wWVKLc';
-const {Telegraf} = require ('telegraf');
-const bot = new Telegraf(botToken);
-
-bot.command(['start'],ctx =>{
-    ctx.reply("Bot started");
-})
-
-bot.launch();
--- botCodes/sample.js
+++ botCodes/sample.js
@@ -6,4 +6,5 @@ bot.command(['start'],ctx =>{
     ctx.reply("Bot started");
 })
 
+console.log("Bot started sent the command /start to your bot")
 bot.launch();
